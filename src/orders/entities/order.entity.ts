40d270b9b37b1ca2b38ec10383d47241531acacf
Aditi Parihar <aditiparihar06@gmail.com>
2025-04-24 12:16:54 +0530

    feat(order): create Order entity with basic fields

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -1,1 +1,15 @@
-export class Order {}
+import { Column, Entity, Generated, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name: 'order'})
+export class Order {
+    @Generated('increment')
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    orderDate: Date;
+
+    @Column()
+    status: string;    
+
+}
