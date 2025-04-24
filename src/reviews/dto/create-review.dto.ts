33820bd91334c710c4347e06e0071c39a7b34bfe
Aditi Parihar <aditiparihar06@gmail.com>
2025-04-24 15:28:39 +0530

    feat(review): add CreateReviewDto with rating and comment validation

--- src/reviews/dto/create-review.dto.ts
+++ src/reviews/dto/create-review.dto.ts
@@ -1,1 +1,14 @@
-export class CreateReviewDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { MinLength } from "class-validator";
+
+export class CreateReviewDto {
+    @ApiProperty() 
+    id: number;
+
+    @ApiProperty()
+    rating: number;
+
+    @ApiProperty()
+    @MinLength(10)
+    comment: string;
+}
