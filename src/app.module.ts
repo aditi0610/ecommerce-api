8c485fce885b4213136937d924b09993e8bf093a
Aditi Parihar <aditiparihar06@gmail.com>
2025-04-24 11:18:40 +0530

    Remove app controller and service file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
@@ -33,7 +31,6 @@ dotenv.config();
     PaymentsModule,
     ReviewsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+
 })
 export class AppModule {}
