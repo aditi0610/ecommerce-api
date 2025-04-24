4dc081e267b20babc2fc90d22c67acb30fb926f1
Aditi Parihar <aditiparihar06@gmail.com>
2025-04-24 12:48:58 +0530

    feat(product): create Product entity with basic fields

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,20 @@
-export class Product {}
+import { Column, Entity, Generated, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name: 'product'})
+export class Product {
+    @Generated('increment')
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    price: number;
+
+    @Column()
+    stock: number;
+}
