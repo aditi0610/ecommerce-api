7fedd2353835b1da4f69fd6862c9c13932657c78
Aditi Parihar <aditiparihar06@gmail.com>
2025-04-24 11:52:12 +0530

    feat(user): create User entity with basic fields

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,17 @@
-export class User {}
+import { Column, Entity, Generated, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name:'user'})
+export class User {
+    @Generated('increment')
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+}
