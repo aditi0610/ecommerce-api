768fccbe41a58d415d55bdb798fc0ec1956edec2
Aditi Parihar <aditiparihar06@gmail.com>
2025-04-24 12:43:34 +0530

    feat(review): create Review entity with basic fields

--- src/reviews/entities/review.entity.ts
+++ src/reviews/entities/review.entity.ts
@@ -1,1 +1,14 @@
-export class Review {}
+import { Column, Entity, Generated, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name: 'review'})
+export class Review {
+    @Generated('increment')
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    rating: number;
+
+    @Column()
+    comment: string;
+}
