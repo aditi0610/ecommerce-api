e3d9393cb7132c4bd677f7df1298fef238c0f85a
Aditi Parihar <aditiparihar06@gmail.com>
2025-04-24 16:59:03 +0530

    feat(product): add CreateProductDto with validation and Swagger docs

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,24 @@
-export class CreateProductDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateProductDto {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  description: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  price: number;
+
+  @IsNotEmpty()
+  stock: number;
+
+ 
+}
