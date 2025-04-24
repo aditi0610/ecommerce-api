8253082f7f7fe2b6c3bf3d063ebe9463b71d3b79
Aditi Parihar <aditiparihar06@gmail.com>
2025-04-24 16:47:33 +0530

    feat(order): add CreateOrderDto with optional status field

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,10 @@
-export class CreateOrderDto {}
+    import { IsString, IsOptional } from 'class-validator';    import { ApiProperty } from '@nestjs/swagger';
+    
+    export class CreateOrderDto {
+      @ApiProperty({ example: 'pending', description: 'Status of the order' })
+      @IsString()
+      @IsOptional() // Optional if you're setting a default status in the service
+      status?: string;
+    }
+    
+
