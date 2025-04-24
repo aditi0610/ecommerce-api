ba95a81f61782d47c1703b8c8ce4572205c8b06d
Aditi Parihar <aditiparihar06@gmail.com>
2025-04-24 10:48:47 +0530

    chore(swagger): import SwaggerModule and DocumentBuilder

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 
 
 
 
 async function bootstrap() {
-  
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Ecommerce APIs')
+    .setDescription('The ecommerce API description')
+    .setVersion('1.0')
+    .addTag('ecommerce')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
