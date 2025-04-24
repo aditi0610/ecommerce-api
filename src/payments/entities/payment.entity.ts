3cbde53b474b9413234af7378714b2047381757a
Aditi Parihar <aditiparihar06@gmail.com>
2025-04-24 12:24:05 +0530

    feat(payment): create Payment entity with basic fields

--- src/payments/entities/payment.entity.ts
+++ src/payments/entities/payment.entity.ts
@@ -1,1 +1,14 @@
-export class Payment {}
+import { Column, Entity, Generated, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name: 'payment'})
+export class Payment {
+    @Generated('increment')
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    method: string; // e.g. "card", "paypal"
+
+    @Column()
+    status: string;  
+}
