7aaf54204747d49585615779293a26a1d091ba27
Aditi Parihar <aditiparihar06@gmail.com>
2025-04-25 18:56:18 +0530

    feat(order): implement create method in OrdersService

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -3,6 +3,7 @@ import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { Order } from './entities/order.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Order])],
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,26 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class OrdersService {
-  create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
+  constructor(
+    @InjectRepository(Order) private orderRepo: Repository<Order>,
+  ){}
+  create(dto: CreateOrderDto) {
+    const order = this.orderRepo.create(dto);
+    return this.orderRepo.save(order);
   }
 
   findAll() {
-    return `This action returns all orders`;
+    return this.orderRepo.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} order`;
+    return this.orderRepo.findOne({ where: { id }});
   }
 
-  update(id: number, updateOrderDto: UpdateOrderDto) {
-    return `This action updates a #${id} order`;
+  update(id: number, dto: UpdateOrderDto) {
+    return this.orderRepo.update(id, dto);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} order`;
+    return this.orderRepo.delete(id);
   }
 }
