f67e1497b86603632319d61ab04b53f8979e0061
Aditi Parihar <aditiparihar06@gmail.com>
2025-04-25 15:54:01 +0530

    feat(product): implement create with category check and eager loading

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,19 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
+import { Category } from 'src/categories/entities/category.entity';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @InjectRepository(Product) private productRepo: Repository<Product>,
+    @InjectRepository(Category) private categoryRepo: Repository<Category>,
+  ){}
+
+  async create(dto: CreateProductDto) {
+    const category = await this.categoryRepo.findOne({ where: { id: dto.categoryId}});
+    if (!category) {
+      throw new NotFoundException('Category not found');
+    }
+    const product = this.productRepo.create({...dto, category });
+    return this.productRepo.save(product);
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.productRepo.find({ relations: ['category'] });
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} product`;
+    return this.productRepo.findOne({ where: { id }, relations: ['category'] });
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
