52b50f9b6bd3a9ee5cbb783011a6792152ac3015
Aditi Parihar <aditiparihar06@gmail.com>
2025-04-25 18:42:26 +0530

    chore(cart): register Cart, User, and Product entities in module

--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -3,9 +3,11 @@ import { CartService } from './cart.service';
 import { CartController } from './cart.controller';
 import { Cart } from './entities/cart.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { Product } from 'src/products/entities/product.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Cart])],
+  imports: [TypeOrmModule.forFeature([Cart,User, Product])],
   controllers: [CartController],
   providers: [CartService],
 })
