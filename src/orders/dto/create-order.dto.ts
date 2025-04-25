7543cd80c14f7d4a401547cb52033a739b1ca49a
Aditi Parihar <aditiparihar06@gmail.com>
2025-04-25 18:50:55 +0530

    feat(order): add userId and paymentId fields with validation and Swagger metadata

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,9 +1,20 @@
-    import { IsString, IsOptional } from 'class-validator';    import { ApiProperty } from '@nestjs/swagger';
+    import { IsString, IsOptional, IsNumber } from 'class-validator';    import { ApiProperty } from '@nestjs/swagger';
     
     export class CreateOrderDto {
       @ApiProperty()
       id: number;
       
+      @ApiProperty()
+      orderDate: Date;
+
+      @ApiProperty()
+      @IsNumber()
+      userId: number;
+
+      @ApiProperty()
+      @IsNumber()
+      paymentId: number;
+
       @ApiProperty({ example: 'pending', description: 'Status of the order' })
       @IsString()
       @IsOptional() // Optional if you're setting a default status in the service
