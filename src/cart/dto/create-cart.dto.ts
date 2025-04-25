70d29e44581906e446a5bc6f8b78e33e2c245b58
Aditi Parihar <aditiparihar06@gmail.com>
2025-04-25 16:50:07 +0530

    feat(cart): add productId field with validation and Swagger metadata

--- src/cart/dto/create-cart.dto.ts
+++ src/cart/dto/create-cart.dto.ts
@@ -1,10 +1,14 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsInt, Min } from "class-validator";
+import { IsInt, IsNumber, Min } from "class-validator";
 
 export class CreateCartDto {
     @ApiProperty()
     id: number;
-
+    
+    @ApiProperty()
+    @IsNumber()
+    productId: number;
+    
     @ApiProperty()
     @IsInt()
     @Min(1)
