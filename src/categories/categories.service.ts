ae067f126b30d04f13c597b399a56f2b63f170f5
Aditi Parihar <aditiparihar06@gmail.com>
2025-04-25 17:41:56 +0530

    feat(category): implement category creation logic

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,27 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from './entities/category.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CategoriesService {
-  
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+  constructor(
+    @InjectRepository(Category) private categoryRepo: Repository<Category>,
+  ){}
+
+  create(dto: CreateCategoryDto) {
+    const category = this.categoryRepo.create(dto);
+    return this.categoryRepo.save(category);
   }
 
   findAll() {
-    return `This action returns all categories`;
+    return this.categoryRepo.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} category`;
+    return this.categoryRepo.findOne({ where: { id } });
   }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
+  update(id: number, dto: UpdateCategoryDto) {
+    return this.categoryRepo.update(id, dto);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} category`;
+    return this.categoryRepo.delete(id);
   }
 }
